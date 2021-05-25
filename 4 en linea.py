09d727d997c614d3d56942576972b2c05b2b60c6
Laureano Diez <contactolaureanodiez@gmail.com>
2021-05-25 18:42:00 -0300

    Todo Contenido + marco del tablero

--- 4 en linea.py
+++ 4 en linea.py
@@ -12,6 +12,44 @@ def tableroVacio():
         [0, 0, 0, 0, 0, 0, 0],
     ]
 
+
+def contenidoColumna(nro_columna, tablero):
+    columna=[]
+    for fila in tablero:
+        celda = fila[nro_columna-1]
+        columna.append(celda)
+    return columna
+
+def contenidoFila(nro_fila, tablero):
+    fila=[]
+    for columna in tablero:
+        celda = columna[nro_fila-1]
+        fila.append(celda)
+    return fila
+
+def todoContenido(opcion, tablero):
+    contador=0
+    if opcion == 1:
+        fila=[]
+        for columna in tablero:
+            for celda in columna:
+                contador=columna[celda]
+                fila.append(contador)
+        return fila
+    else:
+        if opcion == 2:
+            columna=[]
+            for fila in tablero:
+                for celda in fila:
+                    contador=fila[celda]
+                    columna.append(contador)
+            return columna
+        else:
+            print("El valor elegido no es válido. Por favor elija el valor correspondiente: ")
+            contador=9
+            return contador
+
+
 def soltarFichaEnColumna(ficha, columna, tablero):
     for fila in range(6, 0, -1):
         if tablero[fila - 1][columna - 1] == 0:
@@ -24,8 +62,12 @@ def completarTableroEnOrden(secuencia, tablero):
         soltarFichaEnColumna(fichaNumero, columna, tablero)
     return tablero
 
+
 def dibujarTablero(tablero):
+    print(f' {Fore.GREEN}+-- -- -- -- -- -- -- --+{Style.RESET_ALL} ', end='')
+    print('')
     for fila in tablero:
+        print(f' {Fore.GREEN}|{Style.RESET_ALL} ', end='')
         for celda in fila:
             if celda == 0:
                 print(f' {Fore.RED}0{Style.RESET_ALL} ', end='')
@@ -34,7 +76,11 @@ def dibujarTablero(tablero):
                     print(f' {Fore.YELLOW}%s{Style.RESET_ALL} ' %celda, end='')
                 else:
                     print(f' {Fore.BLUE}%s{Style.RESET_ALL} ' %celda, end='')
+        print(f' {Fore.GREEN}|{Style.RESET_ALL} ', end='')
         print('')
+    print(f' {Fore.GREEN}+-----------------------+{Style.RESET_ALL} ', end='')
+    print('')
+
 
 def maxColumnas(secuencia):
     for columna in secuencia:
@@ -44,10 +90,22 @@ def maxColumnas(secuencia):
     return True
 
 
-secuencia = [1, 2, 3, 1, 4]
+secuencia = [1, 2, 3, 1, 4, 6, 9]
+tablero = []
 
-if maxColumnas(secuencia):
-    dibujarTablero(completarTableroEnOrden(secuencia, tableroVacio()))
+
+
+print("Presione (1) si desea mostrar el tablero completo, (2) si desea mostrar SOLO las columnas y (3) si desea mostrar SOLO las filas: ", end='')
+opcion=input()
+if opcion == 1:
+    if maxColumnas(secuencia):
+        tablero = dibujarTablero(completarTableroEnOrden(secuencia, tableroVacio()))
+    else:
+        input('Presione cualquier tecla para continuar.')
+        system('cls')
 else:
-    input('Presione cualquier tecla para continuar.')
-    system('cls')
+    print(todoContenido(opcion, completarTableroEnOrden(secuencia, tableroVacio())))
+
+'''print(contenidoColumna(1, tablero))
+print(contenidoFila(1, tablero))'''
+
